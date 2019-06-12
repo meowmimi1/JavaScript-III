40d4c809385acd484b3fb2a929e476848874cb85
Mimi Hoang <miminhoang1@gmail.com>
2019-06-12 18:29:56 -0400

    Project done

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,13 +15,28 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-
+function GameObject(stuff){
+  this.createdAt = stuff.createdAt,
+  this.name = stuff.name,
+  this.dimensions = stuff.dimensions
+};
+GameObject.prototype.destroy = function(){
+  return `${this.name} was removed from the game.`
+}
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(material){
+  GameObject.call(this, material),
+this.healthPoints = material.healthPoints
+};
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage`
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +47,16 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+ function Humanoid(things){
+   CharacterStats.call(this, things),
+   this.team = things.team,
+   this.weapons = things.weapons,
+   this.language = things.language
+ };
+  Humanoid.prototype = Object.create(CharacterStats.prototype);
+  Humanoid.prototype.greet = function(){
+    return `${this.name} offers a greeting in ${this.language}`
+  }
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +65,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -50,7 +74,7 @@
       height: 1,
     },
     healthPoints: 5,
-    name: 'Bruce',
+    name: 'David',
     team: 'Mage Guild',
     weapons: [
       'Staff of Shamalama',
@@ -102,7 +126,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
